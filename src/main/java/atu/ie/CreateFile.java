c7f83adc0e8d8fdf97681ae9a4e5d4dae8eec1e4
Felipe <g00351833@atu.ie>
2022-12-11 22:21:58 +0000

    file writer to write to file

--- src/main/java/atu/ie/CreateFile.java
+++ src/main/java/atu/ie/CreateFile.java
@@ -6,5 +6,15 @@ public class CreateFile {
 
         File myFile = new File("MyFile.txt");
         System.out.println("My File is located at" + myFile.getAbsolutePath());
+        try{
+            FileWriter myWriter = new FileWriter(myFile);//would need (myFile, true) to keep data
+            myWriter.write("Knuckle");//permanently rewriting
+            myWriter.write("\nappended at end?");//canny use println, need \n for new line
+            myWriter.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
